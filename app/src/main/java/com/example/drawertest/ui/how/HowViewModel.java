6aff76629b49e907a0e082f30d2577953ad1ddfe
prashantupreti <38235022+prashantupreti@users.noreply.github.com>
2021-11-17 09:41:57 -0600

    final update

--- app/src/main/java/com/example/drawertest/ui/how/HowViewModel.java
+++ app/src/main/java/com/example/drawertest/ui/how/HowViewModel.java
@@ -11,8 +11,8 @@ public class HowViewModel extends ViewModel {
     public HowViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("The Home Weather Station Android App works as a Graphic User Interface (GUI) for all the data gathered by the Home Weather Station’s hardware system into a server database. The latest data is displayed at the top in the four blocks. Other old and logged data are shown below in a card view under the four blocks.\n\n" +
-                "We will update more data here describing the hardware and software configurations after we are done with the Interim reports. \n\n" +
-                "Thank You!!!");
+                "Now the logged data can be filtered by date and viewed by our user in a well organized manner. \n\n" +
+                "GUI Developed by: Prashant Upreti");
     }
 
     public LiveData<String> getText() {
